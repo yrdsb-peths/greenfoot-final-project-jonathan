c770b554138ef955c430761380df3b7967c19cbc
340802040 <340802040@gapps.yrdsb.ca>
2022-05-31 10:44:35 -0400

    Ver2

--- MainCharacter.java
+++ MainCharacter.java
@@ -1,38 +1,72 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MainCharacter here.
+ * The Main Character's animations and movements in the normal world
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Jonathan Zhao
+ * @version 2
  */
 public class MainCharacter extends Actor
 {
+    SimpleTimer animationTimer = new SimpleTimer();
+    GreenfootImage[] idle = new GreenfootImage[7];
+    
     /**
-     * Act - do whatever the MainCharacter wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Character Animations
      */
+    public MainCharacter()
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            idle[i] = new GreenfootImage("images/Walk00" + i + ".png");
+            idle[i].scale(50,70);
+        }
+    }
+    
+    int imageIndex = 0;
+    public void animateWalkMainCharacter()
+    {
+        if(animationTimer.millisElapsed() < 90)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("a"))
         {
             setRotation(180);
             move(3);
+            animateWalkMainCharacter();
         }
         else if(Greenfoot.isKeyDown("d"))
         {
             setRotation(0);
             move(3);
+            animateWalkMainCharacter();
         }
-        if(Greenfoot.isKeyDown("w"))
+
+        else if(Greenfoot.isKeyDown("w"))
         {
             setRotation(270);
             move(3);
+            animateWalkMainCharacter();
         }
         else if(Greenfoot.isKeyDown("s"))
         {
             setRotation(90);
             move(3);
+            animateWalkMainCharacter();
+        }
+        else
+        {
+            setImage("images/MainIdle.png");
+            
         }
     }
 }
